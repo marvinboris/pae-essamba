f1f585814b5654c3a42768a1968b314f05e7e1bc
Boris <jaris.ultio.21@gmail.com>
2019-08-23 10:01:21 +0100

    Just started

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/132ff047cd.js"></script>
+    <title>Pae Essamba</title>
+</head>
+<body>
+    <aside style="background-color: #020e1b; width: 250px;">
+        <div class="row">
+            <div class="col-12 d-flex align-items-center" style="height: 78px;">
+                <div class="col-12 border-bottom">
+                    <img src={{ asset('assets/images/logo.png') }} alt="Logo Pae Essamba" class="mr-2">
+                    <div class="text-success">
+                        Pae<br>Essamba
+                    </div>
+                </div>
+            </div>
+    
+            <div class="col-12 d-flex align-items-center">
+                <div class="col-12 border-bottom">
+                    <div class="rounded-circle border-success p-2 mr-3">
+                        <img src={{ "https://placehold.it/100x100" }} alt="Felicity Doe" class="w-100 h-100" style="object-fit: cover; object-position: center;">
+                    </div>
+                    <div>
+                        <div class="mr-2 text-white">
+                            <strong>Felicity Doe</strong>
+                        </div>
+                        <div>
+                            <i class="fas fa-circle text-warning mr-2"></i>
+                            <span class="text-success">
+                                Admin
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="col-12">
+                <nav>
+                    <div class="py-3 px-4 border-left border-success" style="background-color: #011226; border-width: 15px;">
+                        <i class="fas fa-tachometer-alt text-success" style="width: 30px;"></i>Dashboard
+                    </div>
+        
+                    <div class="py-3 px-4 border-left border-success" style="border-width: 15px;">
+                        <i class="fas fa-book-open text-success" style="width: 30px;"></i>Pages
+                    </div>
+        
+                    <div class="py-3 px-4 border-left border-success" style="border-width: 15px;">
+                        <i class="fas fa-calendar-check text-success" style="width: 30px;"></i>Pae Events
+                    </div>
+        
+                    <div class="py-3 px-4 border-left border-success" style="border-width: 15px;">
+                        <i class="fas fa-edit text-success" style="width: 30px;"></i>Projects
+                    </div>
+        
+                    <div class="py-3 px-4 border-left border-success" style="border-width: 15px;">
+                        <i class="fas fa-comment text-success" style="width: 30px;"></i>Notifications
+                    </div>
+        
+                    <div class="py-3 px-4 border-left border-success" style="border-width: 15px;">
+                        <i class="far fa-envelope text-success" style="width: 30px;"></i>Contact Queries
+                    </div>
+        
+                    <div class="py-3 px-4 border-left border-success" style="border-width: 15px;">
+                        <i class="fas fa-cog text-success" style="width: 30px;"></i>Settings
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </aside>
+    
+    <div style="width: calc(100% - 250px);">
+        <header style="background-color: #022043; height: 78px;">
+            <div class="col-12">
+    
+            </div>
+        </header>
+        
+        <main>
+        
+        </main>
+    </div>
+</body>
+</html>
